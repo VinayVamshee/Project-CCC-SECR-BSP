7837f87fce5fce7b05020fd7a2f0d1f0147e937c
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2024-01-26 23:02:59 +0530

    Update Videos.js

--- client/src/Components/Videos.js
+++ client/src/Components/Videos.js
@@ -15,6 +15,18 @@ export default function Videos() {
     }
   }, [])
 
+  const [IsStaffLoggedIn, setIsStaffLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('Stafftoken');
+    if (token) {
+      setIsStaffLoggedIn(true)
+    }
+    else {
+      setIsStaffLoggedIn(false)
+    }
+  }, [])
+
   const [Videos, setVideos] = useState({
     Name: '',
     Link: '',
@@ -125,144 +137,150 @@ export default function Videos() {
       <div className='Search'>
         <input value={SearchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search Video ...' />
       </div>
+      {
+        IsStaffLoggedIn ?
+          <>
+            <div className='Videos-Categories'>
+              {
+                AllVideoCategory.map((Element, idx) => {
+                  const collapseId = `collpseSearchCategory${idx}`;
+                  return (
+                    <div className='Category' key={idx}>
+                      <button className="btn btn-category" onClick={ClearSearch} type="button" data-bs-toggle="collapse" data-bs-target={`#${collapseId}`} aria-expanded="false" aria-controls={collapseId}>
+                        {Element.Name}
+                      </button>
+                      {
+                        IsLoggedIn ?
+                          <button className='btn btn-outline-danger' onClick={() => DeleteVideoCategory(Element._id)}>Delete</button>
+                          :
+                          null
+                      }
 
-      <div className='Videos-Categories'>
-        {
-          AllVideoCategory.map((Element, idx) => {
-            const collapseId = `collpseSearchCategory${idx}`;
-            return (
-              <div className='Category' key={idx}>
-                <button className="btn btn-category" onClick={ClearSearch} type="button" data-bs-toggle="collapse" data-bs-target={`#${collapseId}`} aria-expanded="false" aria-controls={collapseId}>
-                  {Element.Name}
-                </button>
+                      <div className="collapse" id={collapseId}>
+                        <div className="card card-body">
+                          {
+                            AllVideos && AllVideos.filter((video) => video.Category === Element.Name).slice().reverse().map((CategoryElement, videoIdx) => {
+                              return (
+                                <div key={videoIdx}>
+                                  <button className='btn' value={CategoryElement.Name} onClick={CategorySearch}>{CategoryElement.Name}</button>
+                                </div>
+                              )
+                            })
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+
+            <div className='card card-body'>
+              <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 Video-Card">
                 {
-                  IsLoggedIn ? 
-                  <button className='btn btn-outline-danger' onClick={()=>DeleteVideoCategory(Element._id)}>Delete</button>
+                  AllVideos && AllVideos.filter((video) => video.Name.toLowerCase().includes(SearchQuery.toLowerCase())).slice().reverse().map((Element, idx) => {
+                    const animationDelay = `${idx * 0.1}s`;
+                    return (
+                      <div className="col" key={idx}>
+                        <div className="card PopRight shadow-sm" style={{ animationDelay }}>
+                          <iframe title='Videos' src={Element.Link} width="100%" height="360" alt='...' />
+                          <div className="card-body">
+                            <p className="card-text">{Element.Name}</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                              <div className="btn-group">
+                                <a href={Element.Link} target='_blank' rel="noreferrer" className="btn btn-sm btn-outline-primary">View</a>
+                                {
+                                  IsLoggedIn ?
+                                    <button className='btn btn-sm btn-outline-danger' onClick={() => DeleteVideo(Element._id)}>Delete</button>
+                                    :
+                                    null
+                                }
+
+                              </div>
+                              <small className="text-body-secondary">{Element.TimeAdded}</small>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+
+            <div className='AddNew'>
+              {
+                IsLoggedIn ?
+                  <>
+                    <button className='btn btn-warning' data-bs-toggle="modal" data-bs-target="#AddNewVideoModal">Add New Video</button>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AddNewVideoCategoryModal">
+                      Add New Category
+                    </button>
+                  </>
                   :
                   null
-                }
-                
-                <div className="collapse" id={collapseId}>
-                  <div className="card card-body">
-                    {
-                      AllVideos && AllVideos.filter((video) => video.Category === Element.Name).slice().reverse().map((CategoryElement, videoIdx) => {
-                        return (
-                          <div key={videoIdx}>
-                            <button className='btn' value={CategoryElement.Name} onClick={CategorySearch}>{CategoryElement.Name}</button>
-                          </div>
-                        )
-                      })
-                    }
+              }
+
+
+              <div className="modal fade" id="AddNewVideoModal" tabIndex="-1" aria-labelledby="AddNewVideoModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <form onSubmit={AddNewVideo}>
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="AddNewVideoModalLabel">Add New Video</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div className="modal-body">
+                        <label>Video Name</label>
+                        <input type='text' value={Videos.Name} onChange={(e) => setVideos({ ...Videos, Name: e.target.value })} placeholder='Name' />
+                        <label>Drive Link</label>
+                        <input type='url' value={Videos.Link} onChange={handleLink} placeholder='Drive Link Only ...' />
+                        <label>Video Category</label>
+                        <select onChange={(event) => setVideos({ ...Videos, Category: event.target.value })}>
+                          <option value="null"> -- Select --</option>
+                          {
+                            AllVideoCategory.map((Element, idx) => {
+                              return (
+                                <option key={idx} value={Element.Name}>{Element.Name}</option>
+                              )
+                            })
+                          }
+                        </select>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" className="btn btn-primary">Add</button>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
-            )
-          })
-        }
-      </div>
 
-      <div className='card card-body'>
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 Video-Card">
-          {
-            AllVideos && AllVideos.filter((video) => video.Name.toLowerCase().includes(SearchQuery.toLowerCase())).slice().reverse().map((Element, idx) => {
-              const animationDelay = `${idx * 0.1}s`;
-              return (
-                <div className="col" key={idx}>
-                  <div className="card PopRight shadow-sm" style={{ animationDelay }}>
-                    <iframe title='Videos' src={Element.Link} width="100%" height="360" alt='...' />
-                    <div className="card-body">
-                      <p className="card-text">{Element.Name}</p>
-                      <div className="d-flex justify-content-between align-items-center">
-                        <div className="btn-group">
-                          <a href={Element.Link} target='_blank' rel="noreferrer" className="btn btn-sm btn-outline-primary">View</a>
-                          {
-                            IsLoggedIn ?
-                              <button className='btn btn-sm btn-outline-danger' onClick={() => DeleteVideo(Element._id)}>Delete</button>
-                              :
-                              null
-                          }
-
-                        </div>
-                        <small className="text-body-secondary">{Element.TimeAdded}</small>
+              <div className="modal fade" id="AddNewVideoCategoryModal" tabIndex="-1" aria-labelledby="AddNewVideoCategoryModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <form onSubmit={AddNewVideoCategory}>
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="AddNewVideoCategoryModalLabel">Add New Category</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                       </div>
-                    </div>
+                      <div className="modal-body">
+                        <label>Category Name</label>
+                        <input value={Category.Name} onChange={(e) => setCateogry({ ...Category, Name: e.target.value })} placeholder='Category Name' />
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" className="btn btn-primary">Add</button>
+                      </div>
+                    </form>
                   </div>
                 </div>
-              )
-            })
-          }
-        </div>
-      </div>
-
-      <div className='AddNew'>
-        {
-          IsLoggedIn ?
-            <>
-              <button className='btn btn-warning' data-bs-toggle="modal" data-bs-target="#AddNewVideoModal">Add New Video</button>
-              <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AddNewVideoCategoryModal">
-                Add New Category
-              </button>
-            </>
-            :
-            null
-        }
-
-
-        <div className="modal fade" id="AddNewVideoModal" tabIndex="-1" aria-labelledby="AddNewVideoModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <form onSubmit={AddNewVideo}>
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5" id="AddNewVideoModalLabel">Add New Video</h1>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                  <label>Video Name</label>
-                  <input type='text' value={Videos.Name} onChange={(e) => setVideos({ ...Videos, Name: e.target.value })} placeholder='Name' />
-                  <label>Drive Link</label>
-                  <input type='url' value={Videos.Link} onChange={handleLink} placeholder='Drive Link Only ...' />
-                  <label>Video Category</label>
-                  <select onChange={(event) => setVideos({ ...Videos, Category: event.target.value })}>
-                    <option value="null"> -- Select --</option>
-                    {
-                      AllVideoCategory.map((Element, idx) => {
-                        return (
-                          <option key={idx} value={Element.Name}>{Element.Name}</option>
-                        )
-                      })
-                    }
-                  </select>
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                  <button type="submit" className="btn btn-primary">Add</button>
-                </div>
-              </form>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className="modal fade" id="AddNewVideoCategoryModal" tabIndex="-1" aria-labelledby="AddNewVideoCategoryModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <form onSubmit={AddNewVideoCategory}>
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5" id="AddNewVideoCategoryModalLabel">Add New Category</h1>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                  <label>Category Name</label>
-                  <input value={Category.Name} onChange={(e) => setCateogry({ ...Category, Name: e.target.value })} placeholder='Category Name' />
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                  <button type="submit" className="btn btn-primary">Add</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+          </>
+          :
+          null
+      }
     </div>
   )
 }
