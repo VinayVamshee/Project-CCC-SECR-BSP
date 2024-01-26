89c09331ef4690ec1393796117cd6327ddee37f2
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2024-01-26 22:59:57 +0530

    Update Books.js

--- client/src/Components/Books.js
+++ client/src/Components/Books.js
@@ -15,6 +15,18 @@ export default function Books() {
     }
   }, [])
 
+  const [IsStaffLoggedIn, setIsStaffLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('Stafftoken');
+    if (token) {
+      setIsStaffLoggedIn(true)
+    }
+    else {
+      setIsStaffLoggedIn(false)
+    }
+  }, [])
+
   const [Books, setBooks] = useState({
     Name: '',
     Link: '',
@@ -144,162 +156,169 @@ export default function Books() {
         <input value={SearchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search Book ...' />
       </div>
 
-      <div className='Books-Categories'>
-        {
-          AllCategory.map((Element, idx) => {
-            const collapseId = `collpseSearchCategory${idx}`;
-            return (
-              <div className='Category' key={idx}>
-                <button className="btn btn-category" onClick={ClearSearch} type="button" data-bs-toggle="collapse" data-bs-target={`#${collapseId}`} aria-expanded="false" aria-controls={collapseId}>
-                  {Element.Name}
-                </button>
-                {
-                  IsLoggedIn ?
-                    <button className='btn btn-outline-danger' onClick={() => DeleteCategory(Element._id)}>Delete</button>
-                    :
-                    null
-                }
-                <div className="collapse" id={collapseId}>
-                  <div className="card card-body">
-                    {
-                      AllBooks && AllBooks.filter((book) => book.Category === Element.Name).slice().reverse().map((CategoryElement, bookIdx) => {
-                        return (
-                          <div key={bookIdx}>
-                            <button className='btn' value={CategoryElement.Name} onClick={CategorySearch}>{CategoryElement.Name}</button>
-                          </div>
-                        )
-                      })
-                    }
-                  </div>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
+      {
+        IsStaffLoggedIn ?
+          <>
+            <div className='Books-Categories'>
+              {
+                AllCategory.map((Element, idx) => {
+                  const collapseId = `collpseSearchCategory${idx}`;
+                  return (
+                    <div className='Category' key={idx}>
+                      <button className="btn btn-category" onClick={ClearSearch} type="button" data-bs-toggle="collapse" data-bs-target={`#${collapseId}`} aria-expanded="false" aria-controls={collapseId}>
+                        {Element.Name}
+                      </button>
+                      {
+                        IsLoggedIn ?
+                          <button className='btn btn-outline-danger' onClick={() => DeleteCategory(Element._id)}>Delete</button>
+                          :
+                          null
+                      }
+                      <div className="collapse" id={collapseId}>
+                        <div className="card card-body">
+                          {
+                            AllBooks && AllBooks.filter((book) => book.Category === Element.Name).slice().reverse().map((CategoryElement, bookIdx) => {
+                              return (
+                                <div key={bookIdx}>
+                                  <button className='btn' value={CategoryElement.Name} onClick={CategorySearch}>{CategoryElement.Name}</button>
+                                </div>
+                              )
+                            })
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
 
-      <div className='card card-body'>
-        {
-          AllBooks && AllBooks.filter((book) => book.Name.toLowerCase().includes(SearchQuery.toLowerCase())).slice().reverse().map((Element, idx) => {
-            const collapseId = `collapseSearchSpecific${idx}`;
-            // const ClustercollapseId = `clustercollapseSearchSpecific${idx}`;
-            const animationDelay = `${idx * 0.1}s`;
-            return (
-              <div className='AllBooks PopIn' style={{ animationDelay }} key={idx}>
+            <div className='card card-body'>
+              {
+                AllBooks && AllBooks.filter((book) => book.Name.toLowerCase().includes(SearchQuery.toLowerCase())).slice().reverse().map((Element, idx) => {
+                  const collapseId = `collapseSearchSpecific${idx}`;
+                  // const ClustercollapseId = `clustercollapseSearchSpecific${idx}`;
+                  const animationDelay = `${idx * 0.1}s`;
+                  return (
+                    <div className='AllBooks PopIn' style={{ animationDelay }} key={idx}>
 
-                <div className='books-button'>
-                   {/* <button className="btn" type="button" data-bs-toggle="collapse" data-bs-target={`#${collapseId}`} aria-expanded="false" aria-controls={collapseId}>
+                      <div className='books-button'>
+                        {/* <button className="btn" type="button" data-bs-toggle="collapse" data-bs-target={`#${collapseId}`} aria-expanded="false" aria-controls={collapseId}>
                     {Element.Name}
                   </button> */}
-                  <a href={Element.Link} target='_blank' rel="noreferrer">{Element.Name}</a>
+                        <a href={Element.Link} target='_blank' rel="noreferrer">{Element.Name}</a>
 
-                  {/* Cluster */}
-                  {/* <button className="btn btn-info" type="button" data-bs-toggle="collapse" data-bs-target={`#${ClustercollapseId}`} aria-expanded="false" aria-controls={ClustercollapseId}>
+                        {/* Cluster */}
+                        {/* <button className="btn btn-info" type="button" data-bs-toggle="collapse" data-bs-target={`#${ClustercollapseId}`} aria-expanded="false" aria-controls={ClustercollapseId}>
                     {Element.ClusterName}
                   </button> */}
 
-                  <p>{new Date(Element.TimeAdded).toLocaleString()}</p>
-                  {
-                    IsLoggedIn ?
-                      <button className='btn btn-danger' onClick={(e) => DeleteBook(Element._id)}>Delete</button>
-                      :
-                      null
-                  }
+                        <p>{new Date(Element.TimeAdded).toLocaleString()}</p>
+                        {
+                          IsLoggedIn ?
+                            <button className='btn btn-danger' onClick={(e) => DeleteBook(Element._id)}>Delete</button>
+                            :
+                            null
+                        }
 
-                </div>
-                <div className="collapse" id={collapseId}>
-                  <div className="card card-body">
-                    <iframe title='CollapseFrame' src={Element.Link} width="100%" height="600px" />
-                  </div>
-                </div>
-                {/* <div className="collapse" id={ClustercollapseId}>
+                      </div>
+                      <div className="collapse" id={collapseId}>
+                        <div className="card card-body">
+                          <iframe title='CollapseFrame' src={Element.Link} width="100%" height="600px" />
+                        </div>
+                      </div>
+                      {/* <div className="collapse" id={ClustercollapseId}>
                   <div className="card card-body">
                     <p>{Element.ClusterName}</p>
                     <iframe title='CollapseFrame' src={Element.ClusterLink} width="100%" height="600px" />
                   </div>
                 </div> */}
 
+                    </div>
+                  )
+                })
+              }
+            </div>
+
+            <div className='AddNew'>
+              {
+                IsLoggedIn ?
+                  <>
+                    <button className='btn btn-warning' data-bs-toggle="modal" data-bs-target="#AddNewBookModal">Add New Book</button>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AddNewCategoryModal">
+                      Add New Category
+                    </button>
+                  </>
+                  :
+                  null
+              }
+
+
+              <div className="modal fade" id="AddNewBookModal" tabIndex="-1" aria-labelledby="AddNewBookModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <form onSubmit={AddNewBook}>
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="AddNewBookModalLabel">Add New Book</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div className="modal-body">
+                        <label>Book Name</label>
+                        <input type='text' value={Books.Name} onChange={(e) => setBooks({ ...Books, Name: e.target.value })} placeholder='Name' />
+                        <label>Drive Link</label>
+                        <input type='url' value={Books.Link} onChange={handleLink} placeholder='Drive Link Only ...' />
+                        <label>Book Category</label>
+                        <select onChange={(event) => setBooks({ ...Books, Category: event.target.value })}>
+                          <option value="null"> -- Select --</option>
+                          {
+                            AllCategory.map((Element, idx) => {
+                              return (
+                                <option key={idx} value={Element.Name}>{Element.Name}</option>
+                              )
+                            })
+                          }
+                        </select>
+                        <label>Cluster Name</label>
+                        <input type='text' value={Books.ClusterName} onChange={(e) => setBooks({ ...Books, ClusterName: e.target.value })} placeholder='Name' />
+                        <label>Cluster Drive Link</label>
+                        <input type='url' value={Books.ClusterLink} onChange={handleClusterLink} placeholder='Drive Link Only ...' />
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" className="btn btn-primary">Add</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
               </div>
-            )
-          })
-        }
-      </div>
 
-      <div className='AddNew'>
-        {
-          IsLoggedIn ?
-            <>
-              <button className='btn btn-warning' data-bs-toggle="modal" data-bs-target="#AddNewBookModal">Add New Book</button>
-              <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AddNewCategoryModal">
-                Add New Category
-              </button>
-            </>
-            :
-            null
-        }
-
-
-        <div className="modal fade" id="AddNewBookModal" tabIndex="-1" aria-labelledby="AddNewBookModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <form onSubmit={AddNewBook}>
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5" id="AddNewBookModalLabel">Add New Book</h1>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <div className="modal fade" id="AddNewCategoryModal" tabIndex="-1" aria-labelledby="AddNewCategoryModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <form onSubmit={AddNewCategory}>
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="AddNewCategoryModalLabel">Add New Category</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div className="modal-body">
+                        <label>Category Name</label>
+                        <input value={Category.Name} onChange={(e) => setCateogry({ ...Category, Name: e.target.value })} placeholder='Category Name' />
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" className="btn btn-primary">Add</button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
-                <div className="modal-body">
-                  <label>Book Name</label>
-                  <input type='text' value={Books.Name} onChange={(e) => setBooks({ ...Books, Name: e.target.value })} placeholder='Name' />
-                  <label>Drive Link</label>
-                  <input type='url' value={Books.Link} onChange={handleLink} placeholder='Drive Link Only ...' />
-                  <label>Book Category</label>
-                  <select onChange={(event) => setBooks({ ...Books, Category: event.target.value })}>
-                    <option value="null"> -- Select --</option>
-                    {
-                      AllCategory.map((Element, idx) => {
-                        return (
-                          <option key={idx} value={Element.Name}>{Element.Name}</option>
-                        )
-                      })
-                    }
-                  </select>
-                  <label>Cluster Name</label>
-                  <input type='text' value={Books.ClusterName} onChange={(e) => setBooks({ ...Books, ClusterName: e.target.value })} placeholder='Name' />
-                  <label>Cluster Drive Link</label>
-                  <input type='url' value={Books.ClusterLink} onChange={handleClusterLink} placeholder='Drive Link Only ...' />
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                  <button type="submit" className="btn btn-primary">Add</button>
-                </div>
-              </form>
+              </div>
             </div>
-          </div>
-        </div>
+          </>
+          :
+          null
+      }
 
 
-
-        <div className="modal fade" id="AddNewCategoryModal" tabIndex="-1" aria-labelledby="AddNewCategoryModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <form onSubmit={AddNewCategory}>
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5" id="AddNewCategoryModalLabel">Add New Category</h1>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                  <label>Category Name</label>
-                  <input value={Category.Name} onChange={(e) => setCateogry({ ...Category, Name: e.target.value })} placeholder='Category Name' />
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                  <button type="submit" className="btn btn-primary">Add</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
